592c89cf8efa920d7e2f6ee375f5b5eade10ce18
Adam Stankiewicz <agstanki@gmail.com>
2025-05-19 01:30:48 -0400

    fix: resume download url

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -26,7 +26,7 @@ export function Sidebar() {
         {/* Download Resume Button */}
         <div className="mt-12">
           <Link
-            to="/public/pdfs/Adam_Stankiewicz_Resume.pdf"
+            to="/pdfs/Adam_Stankiewicz_Resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className="group inline-flex items-center justify-center rounded-md border border-sky-700 bg-sky-700 px-4 py-2 text-sm font-medium text-white shadow-sm transition-all hover:bg-sky-800 hover:border-sky-800 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-sky-500 focus:ring-offset-1"
